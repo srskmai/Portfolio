9c93ca0241492f34ab52a69ac4d25e211afa0cd2
Surasak <surasak.theerathongdee@gmail.com>
2019-06-19 09:03:08 +0700

    update 20190619

---
+++ README.md
@@ -0,0 +1,2 @@
+Please see my portfolio.
+If you are interested, please don't hesitate to contact me by phone number 088-942-9491 or email address surasak.theerathongdee@gmail.com.
---
+++ js/event.js
@@ -0,0 +1,9 @@
+// $('#project_jrs').on("click",function(){
+//   $('#my_modal').modal('show');
+//   // alert("test");
+// })
+
+
+// $('#my_modal').on('show.bs.modal', function(e){
+//
+// });
